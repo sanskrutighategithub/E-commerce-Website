5b1b3030f54eb778292d14c7e79edce75266d818
sanskrutighate <ghatesanskruti@gmail.com>
2023-06-12 13:23:43 +0530

    second commit

--- E_shop/E_shop/settings.py
+++ E_shop/E_shop/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ghatesanskruti@gmail.com'
-EMAIL_HOST_PASSWORD = 'sanskruti25'
+EMAIL_HOST_USER = 'your mail'
+EMAIL_HOST_PASSWORD = 'your password'
 
 
 
